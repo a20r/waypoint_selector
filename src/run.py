c5c4dcf0ff1bfeeb95ec1743ea3a46f92702fe94
Alex Wallar <aw204@st-andrews.ac.uk>
2015-07-17 13:26:16 -0400

    Getting rid of compiled python files

--- src/run.py
+++ src/run.py
@@ -7,25 +7,8 @@ import sys
 import rospy
 import pageserver
 import restful
-import requests
 import config
-from flask import request
 from nav_msgs.msg import OccupancyGrid
-
-
-@config.app.route('/shutdown', methods=["GET"])
-def shutdown_action():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-    return 'Server shutting down...'
-
-
-def get_shutdown_func(host, port):
-    def __inner():
-        requests.get("http://{}:{}/shutdown".format(host, port))
-    return __inner
 
 
 def update_occ_grid(occ_grid):
@@ -39,11 +22,9 @@ if __name__ == "__main__":
         host = sys.argv[1]
         port = int(sys.argv[2])
         rospy.init_node("waypoint_selector", anonymous=False)
-        sd_func = get_shutdown_func(host, port)
-        rospy.on_shutdown(sd_func)
         occ_sub = rospy.Subscriber("topo_occupancy_grid", OccupancyGrid,
                                    update_occ_grid)
-        config.app.run(host=host, port=port)
+        config.app.run(host=host, port=port, use_reloader=False, debug=True, threaded=True)
         rospy.spin()
     else:
         raise Exception("Correct argument form not supplied")
