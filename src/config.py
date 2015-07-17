a96bd73d5eb51a08e984381063a15d59056a080f
Alex Wallar <aw204@st-andrews.ac.uk>
2015-07-17 13:35:15 -0400

    It finally quits gracefully

--- src/config.py
+++ src/config.py
@@ -5,3 +5,4 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 occ_grid = None
+occ_grid_updates = dict()
--- src/run.py
+++ src/run.py
@@ -3,6 +3,7 @@
 __all__ = ["pageserver", "restful"]
 
 
+import signal
 import sys
 import rospy
 import pageserver
@@ -14,17 +15,26 @@ from nav_msgs.msg import OccupancyGrid
 def update_occ_grid(occ_grid):
     if config.occ_grid is None:
         config.occ_grid = occ_grid
-        print "Updated bromigo"
+    else:
+        for i in xrange(len(occ_grid.data)):
+            if not occ_grid.data[i] == config.occ_grid.data[i]:
+                config.occ_grid_updates[i] = occ_grid.data[i]
+
+
+def signal_handler(signal, frame):
+    sys.exit(0)
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
         host = sys.argv[1]
         port = int(sys.argv[2])
+        signal.signal(signal.SIGINT, signal_handler)
         rospy.init_node("waypoint_selector", anonymous=False)
         occ_sub = rospy.Subscriber("topo_occupancy_grid", OccupancyGrid,
                                    update_occ_grid)
-        config.app.run(host=host, port=port, use_reloader=False, debug=True, threaded=True)
+        config.app.run(host=host, port=port, use_reloader=False, debug=True,
+                       threaded=True)
         rospy.spin()
     else:
         raise Exception("Correct argument form not supplied")
